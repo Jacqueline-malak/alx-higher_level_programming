52f35970becdf0553fdaa089beca84d797085cc3
Jacqueline-malak <Jacqueline-malak>
2023-08-11 19:13:17 -0700

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-#!/user/bin/python3
-if ___name___ == "__main__":
+#!/usr/bin/python3
+if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
